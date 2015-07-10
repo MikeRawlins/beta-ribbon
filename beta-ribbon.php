259a2e982a818e02c7bc5f8c8b50ad318df914b4
Mike Rawlins <MikeRawlins@users.noreply.github.com>
2015-07-10 11:17:52 +0100

    Update beta-ribbon.php
    Added URI and licence

--- beta-ribbon.php
+++ beta-ribbon.php
@@ -1,15 +1,28 @@
 <?php
 /**
  * @package beta-ribbon
- * @version 0.1
+ * @version 0.2
  */
 /*
 Plugin Name: Beta Ribbon
-Plugin URI: 
-Description: When activated this will show a BETA ribbon on the top left corner of your website. Forked from Ubuntu Ribbon by Željko Popivoda
+Plugin URI: https://turriffweb.co.uk/wordpress-plugins/beta-ribbon/
+Description: When activated this will show a BETA ribbon on the top left corner of your website.
 Author: Mike Rawlins
 Version: 0.1
-License: 
+License:             
+	DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
 Author URI: 
 */
 
@@ -20,6 +33,6 @@ function render_beta_ribbon() {
 	} else {
 		$padding_top = 0;
 	}
-	echo "class='beta-ribbon' <img src='{$ribbon_url}' alt='This is BETA' style='position: absolute; top: ".$padding_top ."px; left: 0; z-index: 100000;  border:none;' />";
+	echo "class='beta-ribbon' <img src='{$ribbon_url}' alt='This is BETA' style='position: absolute; top: ".$padding_top ."px; left: 0; z-index: 1000;  border:none;' />";
 }
 add_action('wp_footer', 'render_beta_ribbon');
